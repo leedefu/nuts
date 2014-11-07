8af4513687c54c7dc83d0c0b027a8783da8f2199
lidefu <lidefu@pset.suntec.net>
2014-11-07 14:27:40 +0800

    add xml test sub-project

---
+++ xml/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("+++++++++++++++++++++++++++++++\n");
+    printf("a test for xml.\n");
+
+    printf("+++++++++++++++++++++++++++++++\n");
+    return 0;
+}
