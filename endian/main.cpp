1d81ea492effbef7a755f216ac2091e0740b87fc
lidefu <lidefu@iauto.com>
2020-07-14 14:43:52 +0800

    update endian

--- endian/main.cpp
+++ endian/main.cpp
@@ -1,30 +1,151 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int main(int argc, char* argv[])
+struct sBitEndianCheckStuct
 {
-    {
-        int a = 0x87654321;
-        unsigned char* p = (unsigned char*)&a;
-        {
-            char str[20] = { 0 };
-            sprintf(str,"%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
-            printf(str);
-            printf("\n");
-        }
+    unsigned a:2;
+    unsigned b:3;
+    unsigned c:2;
+    unsigned d:1;
+};
 
-        {
-            char str[20] = { 0 };
-            sprintf(str,"0x%x.0x%x.0x%x.0x%x", p[0], p[1], p[2], p[3]);
-            printf(str);
-            printf("\n");
-        }
-    }
+// check bit endian
+static void checkBitEndian()
+{
+    sBitEndianCheckStuct s;
 
-    // method 1
+    // char result = 3;
+    char result = 0x30;
+    memcpy(&s, &result, 1);
+    printf("a=%d, b=%d, c=%d, d=%d\n", s.a, s.b, s.c, s.d);
+
+    // output:
+    // a=0, b=4, c=1, d=0
+
+    // refer to https://www.cnblogs.com/zhangxian/articles/3503460.html
+    // https://blog.csdn.net/frank_jb/article/details/79027711
+    /*
+        Attention: test host is little endian
+
+        [human read] result=0x30:
+        bits
+        00110000
+
+        C语言的位域中，每个field是严格按照bit地址从低到高排列
+        [sBitEndianCheckStuct]在内存中的分布情况 
+        low address >>>>>>>>>>>>>>>>>>>>> high address (bits)
+        |----|----|----|----|----|----|----|----|
+        |----a----|-------b------|----c----|--d-|
+
+
+        [little endian]: result(0x30) in memory
+        low address >>>>>>>>>>>>>>>>>>>>> high address (bits)
+        |----|----|----|----|----|----|----|----|
+        |  0 |  0 |  0 |  0 |  1 |  1 |  0 |  0 |
+        |    |    |    |    |    |    |    |    |
+
+        [sBitEndianCheckStuct] in memory
+        low address >>>>>>>>>>>>>>>>>>>>> high address
+        bit
+        |----|----|----|----|----|----|----|----|
+        |  0 |  0 |  0 |  0 |  1 |  1 |  0 |  0 |
+        |<---a--->|<------b----->|<---c--->|<-d>|
+
+        low address >>>>>>>>>>>>>>>>>>>>> high address
+        |----|----|
+        |  0 |  0 |
+        |<---a--->|
+        according to [little endian]:
+        [human read] a:
+        00
+        
+                  |----|----|----|               
+                  |  0 |  0 |  1 |               
+                  |<------b----->|               
+        [human read] b:
+        100 
+
+                                 |----|----|
+                                 |  1 |  0 |
+                                 |<---c--->|
+        [human read] c:
+        01 
+                                           |----|
+                                           |  0 |
+                                           |<-d>|
+        [human read] d:
+        0 
+
+        a=0;
+        b=4;
+        c=1;
+        d=0;
+        
+
+    */
+    /*
+        [human read] result=0x30:
+        bits
+        00110000
+
+        C语言的位域中，每个field是严格按照bit地址从低到高排列
+        [sBitEndianCheckStuct]在内存中的分布情况 
+        low address >>>>>>>>>>>>>>>>>>>>> high address (bits)
+        |----|----|----|----|----|----|----|----|
+        |----a----|-------b------|----c----|--d-|
+
+
+        [big endian]: result(0x30) in memory
+        low address >>>>>>>>>>>>>>>>>>>>> high address (bits)
+        |----|----|----|----|----|----|----|----|
+        |  0 |  0 |  1 |  1 |  0 |  0 |  0 |  0 |
+        |    |    |    |    |    |    |    |    |
+
+        [sBitEndianCheckStuct] in memory
+        low address >>>>>>>>>>>>>>>>>>>>> high address
+        bit
+        |----|----|----|----|----|----|----|----|
+        |  0 |  0 |  1 |  1 |  0 |  0 |  0 |  0 |
+        |<---a--->|<------b----->|<---c--->|<-d>|
+
+        low address >>>>>>>>>>>>>>>>>>>>> high address
+        |----|----|
+        |  0 |  0 |
+        |<---a--->|
+        according to [little endian]:
+        [human read] a:
+        00
+        
+                  |----|----|----|               
+                  |  1 |  1 |  0 |               
+                  |<------b----->|               
+        [human read] b:
+        110 
+
+                                 |----|----|
+                                 |  0 |  0 |
+                                 |<---c--->|
+        [human read] c:
+        00 
+                                           |----|
+                                           |  0 |
+                                           |<-d>|
+        [human read] d:
+        0 
+
+        a=0;
+        b=6;
+        c=0;
+        d=0;
+        
+
+    */
+}
+
+static void checkBytesEndian_1()
+{
     /* using union to check endian */
-    /***********************************************************/
+
     union {
         int i;
         char c;
@@ -49,11 +170,15 @@ int main(int argc, char* argv[])
     }
 
     printf("char value 0x%0x, char character %c\n", u.c, u.c);
-    /***********************************************************/
+}
 
-    // method 2
-    /***********************************************************/
-    static union { char c[4]; unsigned int myint; } endian_test = { {'l', '?', '?', 'b'} };
+static void checkBytesEndian_2()
+{
+    static union { 
+        char c[4]; 
+        unsigned int myint; 
+    } endian_test = { {'l', '?', '?', 'b'} };
+
 #define ENDIANNESS ((char)endian_test.myint)
 
     printf("ENDIANNESS is %c\n", ENDIANNESS);
@@ -63,27 +188,27 @@ int main(int argc, char* argv[])
     else {
         printf("big endian\n");
     }
-    /***********************************************************/
+}
 
+static void checkBytesEndian_3()
+{
     // method 3 (different with method 2, same as method 1)
-    /***********************************************************/
     /*
-        low address -> high address
-        endiantest 1:
-            |xx|xx|xx|xx|
+       low address -> high address
+       endiantest 1:
+       |xx|xx|xx|xx|
 
-            |xx|xx|xx|xx|
-            ^
-            |
-        reinterpret_cast<char *>(&endiantest)
+       |xx|xx|xx|xx|
+       ^
+       |
+       reinterpret_cast<char *>(&endiantest)
 
-            |xx|
-            ^
-            |
-        *reinterpret_cast<char *>(&endiantest)
-            
-        
-    */
+       |xx|
+       ^
+       |
+     *reinterpret_cast<char *>(&endiantest)
+
+     */
 
     int endiantest = 1;
     // If this fails, see FLATBUFFERS_LITTLEENDIAN above.
@@ -93,8 +218,68 @@ int main(int argc, char* argv[])
     else {
         printf("method 3 big endian\n");
     }
+}
 
-    /***********************************************************/
+static void test()
+{
+    int a = 0x87654321;
+    unsigned char* p = (unsigned char*)&a;
+    {
+        char str[20] = { 0 };
+        sprintf(str,"%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
+        printf(str);
+        printf("\n");
+    }
+
+    {
+        char str[20] = { 0 };
+        sprintf(str,"0x%x.0x%x.0x%x.0x%x", p[0], p[1], p[2], p[3]);
+        printf(str);
+        printf("\n");
+    }
+}
+
+static void usage()
+{
+    printf("usage:\n");
+    printf("\tendian bits\n");
+    printf("\tendian bytes1\n");
+    printf("\tendian bytes2\n");
+    printf("\tendian bytes3\n");
+    printf("\tendian test\n");
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        usage();
+        return 1;
+    }
+
+    if (strcmp(argv[1], "bits") == 0) {
+        // check bits endian
+        checkBitEndian();
+    }
+
+    if (strcmp(argv[1], "bytes1") == 0) {
+        // check bytes endian
+        checkBytesEndian_1();
+    }
+
+    if (strcmp(argv[1], "bytes2") == 0) {
+        // check bytes endian
+        checkBytesEndian_2();
+    }
+
+    if (strcmp(argv[1], "bytes3") == 0) {
+        // check bytes endian
+        checkBytesEndian_3();
+    }
+
+    if (strcmp(argv[1], "test") == 0) {
+        // test
+        test();
+    }
 
     return 0;
 }
